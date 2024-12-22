4f812b1292a02a25cca04efdbda0b9abbe33660c
remi9023 <lch857953@gmail.com>
2024-12-22 13:16:11 +0900

    초기화 버튼 만들기

---
+++ Assets/02.Scripts/ResetButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+
+    public void Reset()
+    {
+        
+    }
+}
