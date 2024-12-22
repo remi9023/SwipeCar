e37f025633e9734af3a910d3e353f6107574a236
remi9023 <lch857953@gmail.com>
2024-12-22 15:28:23 +0900

    소리 한번만 나게

--- Assets/02.Scripts/CarController.cs
+++ Assets/02.Scripts/CarController.cs
@@ -6,29 +6,35 @@ public class CarController : MonoBehaviour
 {
     float speed = 0f; // 현재 속도
     Vector2 startPos;
-    bool isGameStart = false;
+    bool isGameStart = false; // 게임 시작 여부 확인
+
+    void Start()
+    {
+        GetComponent<AudioSource>().Stop(); // 씬이 로드될 때 소리 정지
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
         {
-            startPos = Input.mousePosition; // 클릭했을 때 마우스 좌표 괄호 벗어났을 때 사라지면 안되기 때문에 제일 위에 변수를 선언함.
+            startPos = Input.mousePosition; // 클릭했을 때 마우스 좌표
         }
 
-        if (Input.GetMouseButtonUp(0)) // 마우스 왼쪽 버튼 클릭
+        if (Input.GetMouseButtonUp(0)) // 마우스 왼쪽 버튼 뗌
         {
-            Vector2 endPos = Input.mousePosition;   // 마우스 뗐을 때 좌표 어차피 괄호안에서만 쓸거라 날아가도 괜찮아서 괄호 안에서만 씀.
-            float swipeLength = endPos.x - startPos.x; // 마우스 드래그한 거리
-            swipeLength = Mathf.Abs(swipeLength); // absolute value
-            speed = swipeLength/1500.0f; // 스와이프 길이의 속도를 변경해줌 (1500분의 거리로 비율 조정)
-            GetComponent<AudioSource>().Play(); //효과음 내기
-            isGameStart = true;                             // 
+            Vector2 endPos = Input.mousePosition; // 마우스 뗐을 때 좌표
+            float swipeLength = endPos.x - startPos.x; // 드래그한 거리
+            swipeLength = Mathf.Abs(swipeLength); // 절댓값 처리
+            speed = swipeLength / 1500.0f; // 스와이프 길이에 따라 속도 계산
+
+            if (!GetComponent<AudioSource>().isPlaying && isGameStart == false) // 소리가 재생 중이 아니고 게임이 처음 시작되었을 때만
+            {
+                GetComponent<AudioSource>().Play(); // 효과음 재생
+                isGameStart = true; // 이후부터 소리를 재생하지 않음
+            }
         }
 
-
-        transform.Translate(speed, 0f, 0f);
-        speed *= 0.98f;
-                
+        transform.Translate(speed, 0f, 0f); // 자동차 이동
+        speed *= 0.98f; // 속도 점차 감소
     }
-
 }
-
